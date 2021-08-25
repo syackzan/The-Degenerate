b1ee5f51756b80b7e46afb40cc7ad0249c658de4
Scotty Yackzan <syackzan@gmail.com>
2021-08-25 10:17:31 -0700

    added Soccer api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 console.log("Hello");
 
-var button = document.getElementById("button");
+var buttonBet = document.getElementById("buttonBet");
+var buttonF = document.getElementById("buttonF");
+
 
 function getApi(){
 
     var requestUrl = "https://api.the-odds-api.com/v4/sports/?apiKey=b66e7e8c0ff61e849ff05e77c6e4e2d5&regions=us";
-    var storage = "https://api.the-odds-api.com/v4/sports/soccer_epl/odds/?apiKey=b66e7e8c0ff61e849ff05e77c6e4e2d5&regions=us";
+    //var storage = "https://api.the-odds-api.com/v4/sports/soccer_epl/odds/?apiKey=b66e7e8c0ff61e849ff05e77c6e4e2d5&regions=us";//
 
     fetch(requestUrl)
       .then(function (response) {
@@ -20,4 +22,28 @@ function getApi(){
     });
   }
 
-  button.addEventListener("click", getApi)
+function getFootballApi() {
+
+    fetch("https://soccer-data.p.rapidapi.com/soccer/team/squad?teamId=801", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "soccer-data.p.rapidapi.com",
+            "x-rapidapi-key": "c5acb3456fmsh97e2594e822d81ep1162a1jsncab86d65f643"
+        }
+    })
+    .then(response => {
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+  
+}
+  
+buttonBet.addEventListener("click", getApi);
+buttonF.addEventListener("click", getFootballApi);
+
