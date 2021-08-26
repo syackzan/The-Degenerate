3df4f2f8482b8fe42c504b1b6b429a5d47336f88
Scotty Yackzan <syackzan@gmail.com>
2021-08-26 11:33:46 -0700

    Add new Java File + moved apis to new file + added button functionality to load URL parameter + added if statement to pull parameter from URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,199 +7,34 @@ var buttonS = document.getElementById("buttonS");
 var buttonBase = document.getElementById("buttonBase");
 var buttonBask = document.getElementById("buttonBask");
 var teamContainer = document.getElementById("checkbox");
-var redirectHTML = "./searchHTML";
+var redirectHTML = "./search.html";
 
-//Pulling API for Sports Book & Odds of Games//
-function getApi(){
-
-    var requestUrl = "https://api.the-odds-api.com/v4/sports/?apiKey=b66e7e8c0ff61e849ff05e77c6e4e2d5&regions=us";
-    //var storage = "https://api.the-odds-api.com/v4/sports/soccer_epl/odds/?apiKey=b66e7e8c0ff61e849ff05e77c6e4e2d5&regions=us";//
-
-    fetch(requestUrl)
-      .then(function (response) {
-        console.log(response);
-        if (response.status === 200) {
-          console.log(response.status);
-        }
-        return response.json();
-    })
-    .then (function(data){
-        console.log(data);
-    });
-  }
-
-//Football API Request//
-function getFootballApi() {
-
-    var footballApi = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=NFL";
-
-    fetch(footballApi)
-    .then (function(response){
-        console.log(response)
-
-        return response.json();
-    })
-    .then (function(data){
-        console.log(data)
-        for (var i = 0; i < data.teams.length; i++){
-            var sTeamBox = document.createElement("div");
-            var sTeamLogo = document.createElement("img");
-            var sTeamName = document.createElement("p");
-            var sTeamDescription = document.createElement("p");
-            var sTeamWebsite = document.createElement("a");
-
-            sTeamLogo.src = data.teams[i].strTeamBadge;
-            sTeamName.textContent = data.teams[i].strTream;
-            sTeamDescription.textContent = data.teams[i].strDescriptionEN;
-            sTeamWebsite.textContent = data.teams[i].strWebsite;
-            sTeamWebsite.href = data.teams[i].strWebsite;
-            
-            sTeamBox.setAttribute("style", "margin: 5px; border: 2px solid black");
-            sTeamLogo.setAttribute("style", "width: 50px; height: 50px;")
-            sTeamName.setAttribute("style", "font-weight: bold");
-            sTeamDescription.setAttribute("style", "font-size: 16px")
-            sTeamWebsite.setAttribute("style", "font-size: 16px")
-            
-            teamContainer.appendChild(sTeamBox);
-            sTeamBox.appendChild(sTeamLogo);
-            sTeamBox.appendChild(sTeamName);
-            sTeamBox.appendChild(sTeamDescription);
-            sTeamBox.appendChild(sTeamWebsite);
-        }
-    });
-  
+//Loading Specific Strings in the Parameters//
+function loadFootballHtml () {
+    var football = "football";
+    searchHTML(football);
 }
 
-//Soccer API Request//
-function getSoccerApi(){
-    var soccerApi = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League";
-
-    fetch(soccerApi)
-    .then (function(response){
-        console.log(response)
-
-        return response.json();
-    })
-    .then (function(data){
-        console.log(data);
-        console.log(data);
-        for (var i = 0; i < data.teams.length; i++){
-            var sTeamBox = document.createElement("div");
-            var sTeamLogo = document.createElement("img");
-            var sTeamName = document.createElement("p");
-            var sTeamDescription = document.createElement("p");
-            var sTeamWebsite = document.createElement("a");
-
-            sTeamLogo.src = data.teams[i].strTeamBadge;
-            sTeamName.textContent = data.teams[i].strTream;
-            sTeamDescription.textContent = data.teams[i].strDescriptionEN;
-            sTeamWebsite.textContent = data.teams[i].strWebsite;
-            sTeamWebsite.href = data.teams[i].strWebsite;
-            
-            sTeamBox.setAttribute("style", "margin: 5px; border: 2px solid black");
-            sTeamLogo.setAttribute("style", "width: 50px; height: 50px;")
-            sTeamName.setAttribute("style", "font-weight: bold");
-            sTeamDescription.setAttribute("style", "font-size: 16px")
-            sTeamWebsite.setAttribute("style", "font-size: 16px")
-            
-            teamContainer.appendChild(sTeamBox);
-            sTeamBox.appendChild(sTeamLogo);
-            sTeamBox.appendChild(sTeamName);
-            sTeamBox.appendChild(sTeamDescription);
-            sTeamBox.appendChild(sTeamWebsite);
-        }
-    });
+function loadSoccerHtml(){
+    var soccer = "soccer";
+    searchHTML(soccer);
 }
 
-//Baseball API request//
-function getBaseballApi(){
-    var baseBallApi = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=MLB";
-
-    fetch(baseBallApi)
-    .then (function(response){
-        console.log(response)
-
-        return response.json();
-    })
-    .then (function(data){
-        console.log(data)
-        for (var i = 0; i < data.teams.length; i++){
-            var sTeamBox = document.createElement("div");
-            var sTeamLogo = document.createElement("img");
-            var sTeamName = document.createElement("p");
-            var sTeamDescription = document.createElement("p");
-            var sTeamWebsite = document.createElement("a");
-
-            sTeamLogo.src = data.teams[i].strTeamBadge;
-            sTeamName.textContent = data.teams[i].strTream;
-            sTeamDescription.textContent = data.teams[i].strDescriptionEN;
-            sTeamWebsite.textContent = data.teams[i].strWebsite;
-            sTeamWebsite.href = data.teams[i].strWebsite;
-            
-            sTeamBox.setAttribute("style", "margin: 5px; border: 2px solid black");
-            sTeamLogo.setAttribute("style", "width: 50px; height: 50px;")
-            sTeamName.setAttribute("style", "font-weight: bold");
-            sTeamDescription.setAttribute("style", "font-size: 16px")
-            sTeamWebsite.setAttribute("style", "font-size: 16px")
-            
-            teamContainer.appendChild(sTeamBox);
-            sTeamBox.appendChild(sTeamLogo);
-            sTeamBox.appendChild(sTeamName);
-            sTeamBox.appendChild(sTeamDescription);
-            sTeamBox.appendChild(sTeamWebsite);
-        }
-    });
+function loadBaseballHtml(){
+    var baseball = "baseball";
+    searchHTML(baseball);
 }
 
-//Basketball API request//
-function getBasketballApi(){
-    var basketBallApi = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=NBA";
-
-    searchHTML();
-
-    fetch(basketBallApi)
-    .then (function(response){
-        console.log(response)
-
-        return response.json();
-    })
-    .then (function(data){
-        console.log(data)
-        for (var i = 0; i < data.teams.length; i++){
-            var sTeamBox = document.createElement("div");
-            var sTeamLogo = document.createElement("img");
-            var sTeamName = document.createElement("p");
-            var sTeamDescription = document.createElement("p");
-            var sTeamWebsite = document.createElement("a");
-
-            sTeamLogo.src = data.teams[i].strTeamBadge;
-            sTeamName.textContent = data.teams[i].strTream;
-            sTeamDescription.textContent = data.teams[i].strDescriptionEN;
-            sTeamWebsite.textContent = data.teams[i].strWebsite;
-            sTeamWebsite.href = data.teams[i].strWebsite;
-            
-            sTeamBox.setAttribute("style", "margin: 5px; border: 2px solid black");
-            sTeamLogo.setAttribute("style", "width: 50px; height: 50px;")
-            sTeamName.setAttribute("style", "font-weight: bold");
-            sTeamDescription.setAttribute("style", "font-size: 16px")
-            sTeamWebsite.setAttribute("style", "font-size: 16px")
-            
-            teamContainer.appendChild(sTeamBox);
-            sTeamBox.appendChild(sTeamLogo);
-            sTeamBox.appendChild(sTeamName);
-            sTeamBox.appendChild(sTeamDescription);
-            sTeamBox.appendChild(sTeamWebsite);
-        }
-    });
+function loadBasketballHtml(){
+    var basketball = "basketball";
+    searchHTML(basketball);
 }
-  
 
 //Add event Listeners for various Buttons//
-buttonBet.addEventListener("click", getApi);
-buttonF.addEventListener("click", getFootballApi);
-buttonS.addEventListener("click", getSoccerApi);
-buttonBase.addEventListener("click", getBaseballApi);
-buttonBask.addEventListener("click", getBasketballApi);
+buttonF.addEventListener("click", loadFootballHtml);
+buttonS.addEventListener("click", loadSoccerHtml);
+buttonBase.addEventListener("click", loadBaseballHtml);
+buttonBask.addEventListener("click", loadBasketballHtml);
 
 var checkBox = $("#checkBox");
 
@@ -233,8 +68,9 @@ $("#inlineCheckbox5").append(localStorage.getItem("Hocky"));
 $("#inlineCheckbox6").append(localStorage.getItem("Soccer"));
 
 //Function Go to Search HTML//
-function searchHTML(){
-    location.storage.href = ""
+function searchHTML(sport){
+    var queryString = './search.html?q=' + sport;
+    location.assign(queryString);
 }
 
 //DatePicker Widget//
---
+++ assets/js/search.js
@@ -0,0 +1,207 @@
+var main = document.getElementById("main");
+
+//function to remove url specification into a variable//
+function parameterSelection (){
+    var searchParamsArr = document.location.search.split('=').pop();
+    console.log(searchParamsArr);
+
+    return searchParamsArr;
+}
+
+//Pulling API's and Printing Information on the Console//
+//Order Books API//
+function getApi(){
+
+    var requestUrl = "https://api.the-odds-api.com/v4/sports/?apiKey=b66e7e8c0ff61e849ff05e77c6e4e2d5&regions=us";
+    //var storage = "https://api.the-odds-api.com/v4/sports/soccer_epl/odds/?apiKey=b66e7e8c0ff61e849ff05e77c6e4e2d5&regions=us";//
+
+    fetch(requestUrl)
+      .then(function (response) {
+        console.log(response);
+        if (response.status === 200) {
+          console.log(response.status);
+        }
+        return response.json();
+    })
+    .then (function(data){
+        console.log(data);
+    });
+  }
+
+//Football Teams API Request//
+function getFootballApi() {
+
+    var footballApi = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=NFL";
+
+    fetch(footballApi)
+    .then (function(response){
+        console.log(response)
+
+        return response.json();
+    })
+    .then (function(data){
+        console.log(data)
+        for (var i = 0; i < data.teams.length; i++){
+            var sTeamBox = document.createElement("div");
+            var sTeamLogo = document.createElement("img");
+            var sTeamName = document.createElement("p");
+            var sTeamDescription = document.createElement("p");
+            var sTeamWebsite = document.createElement("a");
+
+            sTeamLogo.src = data.teams[i].strTeamBadge;
+            sTeamName.textContent = data.teams[i].strTream;
+            sTeamDescription.textContent = data.teams[i].strDescriptionEN;
+            sTeamWebsite.textContent = data.teams[i].strWebsite;
+            sTeamWebsite.href = data.teams[i].strWebsite;
+            
+            sTeamBox.setAttribute("style", "margin: 5px; border: 2px solid black");
+            sTeamLogo.setAttribute("style", "width: 50px; height: 50px;")
+            sTeamName.setAttribute("style", "font-weight: bold");
+            sTeamDescription.setAttribute("style", "font-size: 16px")
+            sTeamWebsite.setAttribute("style", "font-size: 16px")
+            
+            main.appendChild(sTeamBox);
+            sTeamBox.appendChild(sTeamLogo);
+            sTeamBox.appendChild(sTeamName);
+            sTeamBox.appendChild(sTeamDescription);
+            sTeamBox.appendChild(sTeamWebsite);
+        }
+    });
+  
+}
+
+//Soccer Teams API Request//
+function getSoccerApi(){
+    var soccerApi = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League";
+
+    fetch(soccerApi)
+    .then (function(response){
+        console.log(response)
+
+        return response.json();
+    })
+    .then (function(data){
+        console.log(data);
+        console.log(data);
+        for (var i = 0; i < data.teams.length; i++){
+            var sTeamBox = document.createElement("div");
+            var sTeamLogo = document.createElement("img");
+            var sTeamName = document.createElement("p");
+            var sTeamDescription = document.createElement("p");
+            var sTeamWebsite = document.createElement("a");
+
+            sTeamLogo.src = data.teams[i].strTeamBadge;
+            sTeamName.textContent = data.teams[i].strTream;
+            sTeamDescription.textContent = data.teams[i].strDescriptionEN;
+            sTeamWebsite.textContent = data.teams[i].strWebsite;
+            sTeamWebsite.href = data.teams[i].strWebsite;
+            
+            sTeamBox.setAttribute("style", "margin: 5px; border: 2px solid black");
+            sTeamLogo.setAttribute("style", "width: 50px; height: 50px;")
+            sTeamName.setAttribute("style", "font-weight: bold");
+            sTeamDescription.setAttribute("style", "font-size: 16px")
+            sTeamWebsite.setAttribute("style", "font-size: 16px")
+            
+            main.appendChild(sTeamBox);
+            sTeamBox.appendChild(sTeamLogo);
+            sTeamBox.appendChild(sTeamName);
+            sTeamBox.appendChild(sTeamDescription);
+            sTeamBox.appendChild(sTeamWebsite);
+        }
+    });
+}
+
+//Baseball Teams API request//
+function getBaseballApi(){
+    var baseBallApi = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=MLB";
+
+    fetch(baseBallApi)
+    .then (function(response){
+        console.log(response)
+
+        return response.json();
+    })
+    .then (function(data){
+        console.log(data)
+        for (var i = 0; i < data.teams.length; i++){
+            var sTeamBox = document.createElement("div");
+            var sTeamLogo = document.createElement("img");
+            var sTeamName = document.createElement("p");
+            var sTeamDescription = document.createElement("p");
+            var sTeamWebsite = document.createElement("a");
+
+            sTeamLogo.src = data.teams[i].strTeamBadge;
+            sTeamName.textContent = data.teams[i].strTream;
+            sTeamDescription.textContent = data.teams[i].strDescriptionEN;
+            sTeamWebsite.textContent = data.teams[i].strWebsite;
+            sTeamWebsite.href = data.teams[i].strWebsite;
+            
+            sTeamBox.setAttribute("style", "margin: 5px; border: 2px solid black");
+            sTeamLogo.setAttribute("style", "width: 50px; height: 50px;")
+            sTeamName.setAttribute("style", "font-weight: bold");
+            sTeamDescription.setAttribute("style", "font-size: 16px")
+            sTeamWebsite.setAttribute("style", "font-size: 16px")
+            
+            main.appendChild(sTeamBox);
+            sTeamBox.appendChild(sTeamLogo);
+            sTeamBox.appendChild(sTeamName);
+            sTeamBox.appendChild(sTeamDescription);
+            sTeamBox.appendChild(sTeamWebsite);
+        }
+    });
+}
+
+//Basketball Teams API request//
+function getBasketballApi(){
+    var basketBallApi = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=NBA";
+
+    fetch(basketBallApi)
+    .then (function(response){
+        console.log(response)
+
+        return response.json();
+    })
+    .then (function(data){
+        console.log(data)
+        for (var i = 0; i < data.teams.length; i++){
+            var sTeamBox = document.createElement("div");
+            var sTeamLogo = document.createElement("img");
+            var sTeamName = document.createElement("p");
+            var sTeamDescription = document.createElement("p");
+            var sTeamWebsite = document.createElement("a");
+
+            sTeamLogo.src = data.teams[i].strTeamBadge;
+            sTeamName.textContent = data.teams[i].strTream;
+            sTeamDescription.textContent = data.teams[i].strDescriptionEN;
+            sTeamWebsite.textContent = data.teams[i].strWebsite;
+            sTeamWebsite.href = data.teams[i].strWebsite;
+            
+            sTeamBox.setAttribute("style", "margin: 5px; border: 2px solid black");
+            sTeamLogo.setAttribute("style", "width: 50px; height: 50px;")
+            sTeamName.setAttribute("style", "font-weight: bold");
+            sTeamDescription.setAttribute("style", "font-size: 16px")
+            sTeamWebsite.setAttribute("style", "font-size: 16px")
+            
+            main.appendChild(sTeamBox);
+            sTeamBox.appendChild(sTeamLogo);
+            sTeamBox.appendChild(sTeamName);
+            sTeamBox.appendChild(sTeamDescription);
+            sTeamBox.appendChild(sTeamWebsite);
+        }
+    });
+}
+
+//Firing & Pulling URL Element//
+parameterSelection()
+
+if (parameterSelection() == "football"){
+    getFootballApi();
+} else if (parameterSelection() == "soccer"){
+    getSoccerApi();
+} else if (parameterSelection() == "baseball"){
+    getBaseballApi();
+} else if (parameterSelection() == "basketball"){
+    getBasketballApi();
+} else {
+    console.log("fail");
+}
