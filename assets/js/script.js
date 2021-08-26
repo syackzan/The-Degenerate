93634720f438a5b97f104ab8a5e3e7de164e4cdf
Scotty Yackzan <syackzan@gmail.com>
2021-08-26 10:02:49 -0700

    update Widgets to include time

--- assets/js/script.js
+++ assets/js/script.js
@@ -233,3 +233,31 @@ $("#inlineCheckbox6").append(localStorage.getItem("Soccer"));
 $( function() {
     $( "#datepicker" ).datepicker();
   } );
+
+//Auto-Fill Time Picker//
+$( function() {
+    var availableTags = [
+      "8:00am",
+      "9:00am",
+      "10:00am",
+      "11:00am",
+      "12:00pm",
+      "1:00pm",
+      "2:00pm",
+      "3:00pm",
+      "4:00pm",
+      "5:00pm",
+      "6:00pm",
+      "7:00pm",
+      "8:00pm",
+      "9:00pm",
+      "10:00pm",
+      "11:00pm",
+      "12:00am",
+      "Now",
+      "Anytime"
+    ];
+    $( "#time" ).autocomplete({
+      source: availableTags
+    });
+  } );
